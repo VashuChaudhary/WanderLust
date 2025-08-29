9e052f7189229b61e8cb62b872d81f1da8e38e54
VashuChaudhary <whitetiger35286@gmail.com>
2025-08-29 20:57:08 +0530

    fixed image issue

--- routes/listings.js
+++ routes/listings.js
@@ -4,6 +4,9 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingsController = require("../controllers/listings.js");
+const multer = require("multer");
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
 
 router
 	.route("/")
@@ -13,6 +16,7 @@ router
 	.post(
 		isLoggedIn,
 		validateListing,
+		upload.single("listing[image]"),
 		wrapAsync(listingsController.createListing)
 	);
 
@@ -27,7 +31,7 @@ router
 	.put(
 		isLoggedIn,
 		isOwner,
-
+		upload.single("listing[image]"),
 		validateListing,
 		wrapAsync(listingsController.updateListing)
 	)
